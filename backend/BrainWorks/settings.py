3e23e656552c630183c6f0733f7161faf30aa795
zangzoo <20211397@sungshin.ac.kr>
2024-07-14 14:56:27 +0900

    Add changes for upload_image and analysis_result templates and Merge

--- backend/BrainWorks/settings.py
+++ backend/BrainWorks/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend', 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR.parent, 'frontend', 'static')]
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
