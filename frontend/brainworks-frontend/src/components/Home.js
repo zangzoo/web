b6d2f5f5c932c88c50d4e24c2243e153b3770770
0gonge <Challengef0802@gmail.com>
2024-08-24 22:22:25 +0900

    [Feat]Header icon 적용 및 링크

--- frontend/brainworks-frontend/src/components/Home.js
+++ frontend/brainworks-frontend/src/components/Home.js
@@ -1,12 +1,25 @@
 // src/components/Home.js
 
 import React from 'react';
+import './Home.css';
 
 function Home() {
     return (
-        <div style={{ textAlign: 'center', padding: '50px' }}>
-            <h1>Welcome to BrainWorks</h1>
-            <p>This is the home screen of your application.</p>
+        <div className="home">
+            <nav className="navbar">
+                <div className="navbar-left">
+                    <img src="/brainlogo.png" alt="Logo" className="logo" /> {/* 아이콘 추가 */}
+                    <h1>BrainWorks</h1>
+                </div>
+                <div className="navbar-right">
+                    <a href="/home">Home</a>
+                    <a href="/login">Login</a>
+                </div>
+            </nav>
+            <div className="content">
+                <h2>Welcome to BrainWorks</h2>
+                <p>This is the home screen of your application.</p>
+            </div>
         </div>
     );
 }
