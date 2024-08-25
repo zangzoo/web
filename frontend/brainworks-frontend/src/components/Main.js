47bccaa85bdc8a20b635d3dce3cee601d2b66ed5
0gonge <Challengef0802@gmail.com>
2024-08-26 04:11:04 +0900

    [Feat] Analysis화면 파일 업로드 구현

--- frontend/brainworks-frontend/src/components/Main.js
+++ frontend/brainworks-frontend/src/components/Main.js
@@ -1,10 +1,43 @@
 // src/components/Main.js
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Main.css';
 
 function Main() {
+    const [selectedFile, setSelectedFile] = useState(null);
+    const navigate = useNavigate();
+
+    const handleFileChange = (event) => {
+        setSelectedFile(event.target.files[0]);
+    };
+
+    const handleSubmit = () => {
+        if (selectedFile) {
+            // 파일 업로드 로직 추가 (필요에 따라)
+            navigate('/analysis');
+        } else {
+            alert('Please select an image.');
+        }
+    };
+
     return (
-        <div>
-            <h1>Welcome to the Main Page!</h1>
+        <div className="main-container">
+            <nav className="navbar">
+                <div className="navbar-left">
+                    <img src="/brainlogo.png" alt="Logo" className="logo" />
+                    <h1>BrainWorks</h1>
+                </div>
+                <div className="navbar-right">
+                    <a href="/home">Home</a>
+                    <a href="/login">Login</a>
+                </div>
+            </nav>
+            <div className="main-content">
+                <h2>Alzheimer detection</h2>
+                <p>Upload Image:</p>
+                <input type="file" onChange={handleFileChange} />
+                <button className="submit-button" onClick={handleSubmit}>Submit</button>
+            </div>
         </div>
     );
 }
