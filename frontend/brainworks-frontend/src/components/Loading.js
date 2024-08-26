0977c8e6945232c2abb9cc3f867628342374d5cb
0gonge <Challengef0802@gmail.com>
2024-08-26 20:41:54 +0900

    [Feat] Loading화면 3D animation 추가

--- frontend/brainworks-frontend/src/components/Loading.js
+++ frontend/brainworks-frontend/src/components/Loading.js
@@ -1,16 +1,56 @@
-// Loading.js
-import React from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, useGLTF, Html } from '@react-three/drei';
 import './Loading.css';
 
+function BrainModel() {
+    const { scene } = useGLTF('/finalbrain.glb');
+    return <primitive object={scene} scale={0.029} />;
+}
+
 function Loading({ patientName, fileName }) {
+    const [progress, setProgress] = useState(0);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setProgress((prevProgress) => {
+                if (prevProgress < 100) {
+                    return prevProgress + 1;
+                } else {
+                    clearInterval(interval);
+                    return 100;
+                }
+            });
+        }, 100);
+
+        const timer = setTimeout(() => {
+            navigate('/analysis', { state: { patientName, fileName } });
+        }, 10000);
+
+        return () => {
+            clearInterval(interval);
+            clearTimeout(timer);
+        };
+    }, [navigate, patientName, fileName]);
+
     return (
         <div className="loading-container">
-            <div className="loading-spinner"></div>
-            <p>
+            <Canvas style={{ height: 500 }}>
+                <Suspense fallback={<Html><span>Loading...</span></Html>}>
+                    <ambientLight intensity={1.5} />
+                    <directionalLight position={[5, 5, 5]} />
+                    <BrainModel />
+                    <OrbitControls enableZoom={false} autoRotate autoRotateSpeed={2} maxDistance={5} />
+                </Suspense>
+            </Canvas>
+            <p className="loading-text">
                 BrainWorks is analyzing based on
                 <span className="patient-name"> {patientName}’s</span>
                 <span className="file-name"> {fileName}</span>.
             </p>
+            <p className="loading-percentage">{progress}%</p>
         </div>
     );
 }
