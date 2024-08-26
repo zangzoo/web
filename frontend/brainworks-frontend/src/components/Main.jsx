0693baed38e910b849dd74b2b476de3f1f95af32
0gonge <Challengef0802@gmail.com>
2024-08-26 15:43:07 +0900

    [Feat] 분석화면에 Patient Select 추가

--- frontend/brainworks-frontend/src/components/Main.jsx
+++ frontend/brainworks-frontend/src/components/Main.jsx
@@ -5,6 +5,7 @@ import './Main.css';
 function Main() {
     const [selectedFile, setSelectedFile] = useState(null);
     const [previewUrl, setPreviewUrl] = useState(null);
+    const [selectedPatient, setSelectedPatient] = useState(""); // 환자 선택 state 추가
     const navigate = useNavigate();
 
     const handleFileChange = (event) => {
@@ -22,11 +23,15 @@ function Main() {
         }
     };
 
+    const handlePatientChange = (event) => {
+        setSelectedPatient(event.target.value);
+    };
+
     const handleSubmit = () => {
-        if (selectedFile) {
+        if (selectedPatient && selectedFile) {
             navigate('/analysis');
         } else {
-            alert('Please select an image.');
+            alert('Please select a patient and an image.');
         }
     };
 
@@ -49,8 +54,28 @@ function Main() {
             </div>
 
             <div className="upload-section">
-                <h2>Upload MRI Scan for Alzheimer's Detection</h2>
+                <h2>Upload an Image for Alzheimer's Prediction</h2>
                 <form>
+                    <label htmlFor="patient-select" className="file-upload-label">
+                        Select Patient:
+                    </label>
+                    <select
+                        id="patient-select"
+                        value={selectedPatient}
+                        onChange={handlePatientChange}
+                        className="patient-select"
+                    >
+                        <option value="">Please select a patient.</option>
+                        <option value="song yeogyeong">song yeogyeong</option>
+                        <option value="김새드">김새드</option>
+                        <option value="김해피">김해피</option>
+                        <option value="김맥주">김맥주</option>
+                        <option value="김술잔">김술잔</option>
+                        <option value="김애플">김애플</option>
+
+                        {/* 환자 목록을 이곳에 추가 */}
+                    </select>
+
                     <label htmlFor="file-upload" className="file-upload-label">
                         Upload Image:
                     </label>
