fe8b2c393324746edf10c01e2eb53c9935fce579
0gonge <Challengef0802@gmail.com>
2024-09-02 05:02:01 +0900

    [Feat] 분석화면 완료

--- frontend/brainworks-frontend/src/App.js
+++ frontend/brainworks-frontend/src/App.js
@@ -8,7 +8,6 @@ import Login from './components/Login';
 import Main from './components/Main';
 import Analysis from './components/Analysis';
 import PatientList from './components/PatientList';
-import PatientDetails from './components/PatientDetails';
 import './components/fonts/aileron/aileron.css';
 
 function App() {
@@ -21,7 +20,6 @@ function App() {
                 <Route path="/main" element={<Main />} />
                 <Route path="/analysis" element={<Analysis />} />
                 <Route path="/patient-list" element={<PatientList />} />
-                <Route path="/patient-details" element={<PatientDetails />} />
             </Routes>
         </Router>
     );
